ca2e9025dca2e1fdf68cfb20dd4961c38852cf8c
Danki120 <brandonnunezquesada@gmail.com>
2025-09-24 00:49:10 -0600

    First test for production build

--- src/components/Rent.jsx
+++ src/components/Rent.jsx
@@ -1,5 +1,18 @@
+import { RENT_ITEMS } from "../rent"
+
+import '../styles/Rent.css'
+
 export default function Rent() {
     return(
-        <h1>penis</h1>
+        <div id="rentContainer">
+            <ul>
+                {RENT_ITEMS.map((item) => (
+                    <li key={item.id}>
+                        <p>{item.itemName}</p>
+                        <p>{item.price}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
---
+++ src/rent.js
@@ -0,0 +1,47 @@
+export const RENT_ITEMS = [
+    {
+        id: 1,
+        itemName: 'BMPCC4k + 18-35',
+        price: '₡45000'
+    },
+    {
+        id: 2,
+        itemName: 'BMPCC4K KIT',
+        price: '₡35000'
+    },
+    {
+        id: 3,
+        itemName: 'BMPCC4k BODY',
+        price: '₡20000'
+    },
+    {
+        id: 4,
+        itemName: 'SIGMA 18-35 F1.8',
+        price: '₡15000'
+    },
+    {
+        id: 5,
+        itemName: 'KIT - MONITOR FEELWORLD 7 PULGADAS',
+        price: '₡14000'
+    },
+    {
+        id: 6,
+        itemName: 'TRIPODE MANFROTTO 290 LIGHT',
+        price: '₡5000'
+    },
+    {
+        id: 7,
+        itemName: 'SAMSUNG T7 SSD',
+        price: '₡5000'
+    },
+    {
+        id: 8,
+        itemName: 'SPEEDBOOSTER EF-MFT x0.71 COMMLITE',
+        price: '₡2500'
+    },
+    {
+        id: 9,
+        itemName: 'STEP UP RINGS',
+        price: '₡500'
+    },
+]
