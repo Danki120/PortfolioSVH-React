49cf595b13420a950d456db19791fab1d417d6cb
Danki120 <brandonnunezquesada@gmail.com>
2025-10-09 20:25:10 -0600

    Changed to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, HashRouter, createHashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import Portfolio from './components/Portfolio.jsx'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <App url='home'/>,
